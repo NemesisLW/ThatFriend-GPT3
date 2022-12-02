a3dd8f190743f0ee6a3f328de3c44eaedb971b3b
NemesisLW <86069181+NemesisLW@users.noreply.github.com>
2022-12-02 22:01:35 +0530

    Latest Commit

---
+++ pages/index.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+
+const Home = () => {
+  const [userInput, setUserInput] = useState("");
+
+  //API Call
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied....", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
+  const onUserChangeText = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  return (
+    <div className="root">
+      <Head>
+        <title>GPT-3 Writer | Arghya</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>The All-Knowing Seer</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Ask away whatever troubles you.</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea
+            placeholder="start typing here"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangeText}
+          />
+
+          <div className="prompt-buttons">
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
+              </div>
+            </a>
+          </div>
+          {/* Output */}
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://twitter.com/Arghyad18"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={buildspaceLogo} alt="buildspace logo" />
+            <p>build by Arghya</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
